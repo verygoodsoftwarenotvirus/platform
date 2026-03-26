282823dc06d8692bf5e0488b7142d1fb7e986ee3
Jeffrey D <1289344+verygoodsoftwarenotvirus@users.noreply.github.com>
2026-03-25 19:58:31 -0500

    just delete the fuckin' thing

--- notifications/doc.go
+++
@@ -1,6 +0,0 @@
-/*
-Package notifications provides a push notification sending interface with implementations for APNs and FCM.
-
-Deprecated: Use mobilenotifications instead. This package will be removed in a future release.
-*/
-package notifications
